a410a80a8ac085e2a00b6316a557ce2611015930
Jose Manuel <jmdcarq89@gmail.com>
2021-03-10 22:12:03 +0100

    color-size prop

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import "./App.scss"
 import {Canvas, useFrame } from "react-three-fiber";
 
 
-const SpinningMesh = ({position}) => {
+const SpinningMesh = ({position, args, color}) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
   return (
     <mesh position={position} ref={mesh}>
-      <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
-      <meshStandardMaterial attach='material' color='lightblue' />
+      <boxBufferGeometry attach='geometry' args={args}/>
+      <meshStandardMaterial attach='material' color={color} />
     </mesh>
   );
 };
@@ -21,9 +21,9 @@ function App() {
     <>
       <Canvas colorManagement camera={{position: [-5,2,10], fov: 60}}>
         <ambientLight intensity={0.3} />
-        <SpinningMesh position={[0,1,0]}/>
-        <SpinningMesh position={[-2,1,-5]}/>
-        <SpinningMesh position={[5,1,-2]}/>
+        <SpinningMesh position={[0,1,0]} args={[3,2,1]} color="lightblue"/>
+        <SpinningMesh position={[-2,1,-5]} color="pink"/>
+        <SpinningMesh position={[5,1,-2]} color="pink"/>
       </Canvas>
     </>
   )
