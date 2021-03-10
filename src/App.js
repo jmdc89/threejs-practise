cc291fb386640dfa2f38c5531d277a78919c860c
Jose Manuel <jmdcarq89@gmail.com>
2021-03-10 22:18:41 +0100

    lights

--- src/App.js
+++ src/App.js
@@ -21,6 +21,20 @@ function App() {
     <>
       <Canvas colorManagement camera={{position: [-5,2,10], fov: 60}}>
         <ambientLight intensity={0.3} />
+        <pointLight position={[-10,0,-20]} intensity={0.5}/>
+        <pointLight position={[0,-10, 0]} intensity={0.5}/>
+        <directionalLight
+          position={[0,10,0]}
+          intensity={1}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+
         <SpinningMesh position={[0,1,0]} args={[3,2,1]} color="lightblue"/>
         <SpinningMesh position={[-2,1,-5]} color="pink"/>
         <SpinningMesh position={[5,1,-2]} color="pink"/>
