e580d42c55737bbbed00e3cf81feb230fcd5dc44
Jose Manuel <jmdcarq89@gmail.com>
2021-03-10 21:37:43 +0100

    spinning box

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./App.scss"
 
-import {Canvas} from "react-three-fiber";
+import {Canvas, useFrame } from "react-three-fiber";
 
+
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
+      <meshStandardMaterial attach='material' />
+    </mesh>
+  );
+};
 
 function App() {
   return (
     <>
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
-          <meshStandardMaterial attach='material' />
-        </mesh>
+        <Box/>
       </Canvas>
     </>
   )
