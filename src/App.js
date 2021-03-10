d4d3cb1f66b7753996e67ab744631f7086b3db9e
Jose Manuel <jmdcarq89@gmail.com>
2021-03-10 21:23:47 +0100

    initial mesh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import "./App.scss"
 
+import {Canvas} from "react-three-fiber";
+
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <>
+      <Canvas>
+        <mesh>
+          <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
+          <meshStandardMaterial attach='material' />
+        </mesh>
+      </Canvas>
+    </>
+  )
 }
 
 export default App;
