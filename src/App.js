31a74acdd979ebd1aef066d06c802eabafa5aea4
Jose Manuel <jmdcarq89@gmail.com>
2021-03-10 22:08:41 +0100

    position prop

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import "./App.scss"
 import {Canvas, useFrame } from "react-three-fiber";
 
 
-const Box = () => {
+const SpinningMesh = ({position}) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
   return (
-    <mesh ref={mesh}>
+    <mesh position={position} ref={mesh}>
       <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
       <meshStandardMaterial attach='material' color='lightblue' />
     </mesh>
@@ -21,7 +21,9 @@ function App() {
     <>
       <Canvas colorManagement camera={{position: [-5,2,10], fov: 60}}>
         <ambientLight intensity={0.3} />
-        <Box/>
+        <SpinningMesh position={[0,1,0]}/>
+        <SpinningMesh position={[-2,1,-5]}/>
+        <SpinningMesh position={[5,1,-2]}/>
       </Canvas>
     </>
   )
