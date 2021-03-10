bac371926379819d19d7f2fb78bef31dd2b9b97b
Jose Manuel <jmdcarq89@gmail.com>
2021-03-10 22:29:12 +0100

    threejs shadows

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const SpinningMesh = ({position, args, color}) => {
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
   return (
-    <mesh position={position} ref={mesh}>
+    <mesh castShadow position={position} ref={mesh}>
       <boxBufferGeometry attach='geometry' args={args}/>
       <meshStandardMaterial attach='material' color={color} />
     </mesh>
@@ -19,11 +19,15 @@ const SpinningMesh = ({position, args, color}) => {
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{position: [-5,2,10], fov: 60}}>
+      <Canvas 
+          shadowMap
+          colorManagement 
+          camera={{position: [-5,2,10], fov: 60}}>
         <ambientLight intensity={0.3} />
         <pointLight position={[-10,0,-20]} intensity={0.5}/>
         <pointLight position={[0,-10, 0]} intensity={0.5}/>
         <directionalLight
+          castShadow
           position={[0,10,0]}
           intensity={1}
           shadow-mapSize-width={1024}
@@ -34,6 +38,16 @@ function App() {
           shadow-camera-top={10}
           shadow-camera-bottom={-10}
         />
+
+        <group>
+          <mesh
+              receiveShadow
+              rotation={[-Math.PI / 2, 0, 0]} 
+              position={[0,-3,0]}>
+            <planeBufferGeometry attach='geometry' args={[100,100]} />
+            <shadowMaterial attach='material' opacity={0.3} />
+          </mesh>
+        </group>
 
         <SpinningMesh position={[0,1,0]} args={[3,2,1]} color="lightblue"/>
         <SpinningMesh position={[-2,1,-5]} color="pink"/>
