c02d5bcab4d58586f6a4d72276339ccd276aa4b0
Jose Manuel <jmdcarq89@gmail.com>
2021-03-20 12:55:12 +0100

    onPointers

--- src/components/shoe/Shoe.js
+++ src/components/shoe/Shoe.js
@@ -1,20 +1,42 @@
 import React, { useRef, useState, Suspense} from 'react';
 import { Canvas, useFrame } from 'react-three-fiber';
 import { useGLTF } from '@react-three/drei';
+import {proxy, useProxy} from "valtio";
+
+const state = proxy({
+    current: null,
+    items: {
+      laces: "#ffffff",
+      mesh: "#ffffff",
+      caps: "#ffffff",
+      inner: "#ffffff",
+      sole: "#ffffff",
+      stripes: "#ffffff",
+      band: "#ffffff",
+      patch: "#ffffff",
+    },
+  });
 
 function Model(props) {
     const group = useRef()
+    const snap = useProxy(state);
     const { nodes, materials } = useGLTF('shoecompress.glb')
     return (
-      <group ref={group} {...props} dispose={null}>
-        <mesh material={materials.mesh} geometry={nodes.shoe.geometry} />
-        <mesh material={materials.sole} geometry={nodes.shoe_1.geometry} />
-        <mesh material={materials.stripes} geometry={nodes.shoe_2.geometry} />
-        <mesh material={materials.band} geometry={nodes.shoe_3.geometry} />
-        <mesh material={materials.caps} geometry={nodes.shoe_4.geometry} />
-        <mesh material={materials.patch} geometry={nodes.shoe_5.geometry} />
-        <mesh material={materials.inner} geometry={nodes.shoe_6.geometry} />
-        <mesh material={materials.laces} geometry={nodes.shoe_7.geometry} />
+        <group
+        ref={ref}
+        dispose={null}
+        onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
+        onPointerOut={(e) => e.intersections.length === 0 && set(null)}
+        onPointerMissed={() => (state.current = null)}
+        onPointerDown={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
+        <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
+        <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
+        <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
+        <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner} />
+        <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole} />
+        <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes} />
+        <mesh geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band} />
+        <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch} />
       </group>
     )
   }
